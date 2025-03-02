4a68071acfddc161d98357eb0a228b5d6ef5dd91
Adrian Santos <asago2002@gmail.com>
2025-03-02 19:10:56 +0100

    elimiando comentarios

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,7 @@
-<?php
+docker <?php
 
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
-//comentario de prueba para git
-//comentario de prueba para git 2
-//comentario de prieba para git 3
